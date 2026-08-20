3e712218a5cc65538c450fbd2f7a8522d2be985a
Praveen744444444 <indupalli24@gmail.com>
2026-08-20 19:09:48 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_WEB_API_KEY",
+  apiKey: "AIzaSyAJWdsNykkCp9tJfoBiZNoVfWT0orM6BHA",
   authDomain: "indupalli-services-18404.firebaseapp.com",
   projectId: "indupalli-services-18404",
   storageBucket: "indupalli-services-18404.firebasestorage.app",
-  messagingSenderId: "187485195719",
-  appId: "1:187485195719:web:a2f422b12c47ea2d549de2"
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID", // Add your actual ID here if you have it
+  appId: "YOUR_APP_ID" // Add your actual ID here if you have it
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
